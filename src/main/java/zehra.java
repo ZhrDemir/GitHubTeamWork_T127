91e4af6e43f321a2702c4ad20dc67ab47da2ea3e
ZhrDemir <zdemir2702@gmail.com>
2023-12-14 11:40:06 +0300

    first commit

---
+++ src/main/java/zehra.java
@@ -0,0 +1,12 @@
+public class zehra {
+    public static void main(String[] args) {
+        System.out.println("selam");
+    }
+
+   git init
+   git add .
+   git commit -m "first commit"
+   git branch -M main
+   git remote add origin https://github.com/ZhrDemir/GitHubTeamWork_T127.git
+   git push -u origin main
+}
